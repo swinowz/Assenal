1fcec5753896131b3e79aa8c54d2a78cbeeeac52
swinowz <anthony.guilbert@campus-cci84.fr>
2024-07-05 16:25:42 +0200

    o

--- assenal.py
+++ assenal.py
@@ -11,6 +11,8 @@ from rich.live import Live
 from rich.table import Table
 from simple_term_menu import TerminalMenu
 import subprocess
+import pyperclip
+from random import *
 
 def signal_handler(sig, frame):
     print('Interrupted')
@@ -30,7 +32,10 @@ def search():
     table.add_column("Command")
     with Live(table, refresh_per_second=4):
         for tool in tools:
-            if usersearch == "*" or usersearch in tool['title'].lower() or usersearch in tool['object']['command'].lower() or usersearch in tool['object']['description'].lower():
+            if (usersearch == "*" or usersearch in tool['title'].lower() or
+               usersearch in tool['object']['command'].lower() or 
+               usersearch in tool['object']['description'].lower() or
+               usersearch in tool['object']['tags'].lower()):
                 finds.append(tool['title'])
                 table.add_row(f"[green]{tool['title']}", f"{tool['object']['description']}", f"{tool['object']['command']}")
 
@@ -52,37 +57,30 @@ def search():
                         subprocess.run(command, shell=True, check=True)
                     case "c":
                         pyperclip.copy(command)
+                        exit(0)
                     case "q":
                         exit(0)
                 
             except subprocess.CalledProcessError as e:
                 print(f"Command failed with error: {e}")
 
+
+def artGen():
+    files = os.listdir("./arts")
+    with open(f"./arts/{randint(1, len(files))}", 'r') as f:
+        return f.read()
+
 def mainMenu(menutext="Assenal", clearscreen=True):
     if clearscreen:
         os.system('clear')
-    house_art = """
+    house_art = artGen()
 
-__
- /  \\
-/____\\
-|    ||
-|_[]_||
-
-    """
-
-    console.print(house_art, style="red", justify="center")
+    console.print(house_art, style="cyan", justify="center")
 
     text = Text("\n" + menutext)
     text.stylize("bold")
     text.stylize("size 40")
     console.print(text, justify="center")
-    layout = Layout()
-    layout.split_column(
-        Layout(name="search"),
-        Layout(name="results")
-    )
-    layout["search"].size = 10
     
 def main():
     search_completed = False
